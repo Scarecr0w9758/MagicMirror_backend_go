e0ded8bb23f5f8077342b5270a662c8ed370436e
demin.m <Matway.demin@yandex.ru>
2024-06-12 23:26:22 +0500

    feat: add email field to userModel

--- models/models.go
+++ models/models.go
@@ -1,28 +1,28 @@
 package models
 
-
-type Tokens struct{
-	Id uint
-	UserId uint
-	AccessToken string
-	RefreshToken string
-	ExpirationDate  int64
+type Tokens struct {
+	Id             uint
+	UserId         uint
+	AccessToken    string
+	RefreshToken   string
+	ExpirationDate int64
 }
 
-type User struct{
-	Id uint  `json:"id"`
-	Login string `json:"login"`
-	Name string `json:"name"`
-	Password string `json:"password"`
+type User struct {
+	Id            uint   `json:"id"`
+	Login         string `json:"login"`
+	Email         string `json:"email"`
+	Name          string `json:"name"`
+	Password      string `json:"password"`
 	RememberToken string `json:"rememberToken"`
 }
 
 type Task struct {
-	Id uint `json:"id"`
-	UserId uint `json:"user_id"`
-	Title string `json:"title"`
-	Description  string `json:"description"`
-	Completed	 bool `json:"completed" sql:"DEFAULT:false;type:boolean;index" gorm:"not null"` 
-	StartDate	 int64 `json:"start_date"`
-	EndDate      int64 `json:"end_date"`
+	Id          uint   `json:"id"`
+	UserId      uint   `json:"user_id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed" sql:"DEFAULT:false;type:boolean;index" gorm:"not null"`
+	StartDate   int64  `json:"start_date"`
+	EndDate     int64  `json:"end_date"`
 }
