4eadc5422e761f6163f34cd5aef17587ff5fe76f
demin.m <Matway.demin@yandex.ru>
2024-06-12 23:27:39 +0500

    fix: controller console error

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,10 +1,6 @@
 package controllers
 
 import (
-	database "magic-mirror/database"
-	"magic-mirror/models"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
@@ -27,28 +23,23 @@ import (
 
 var validate=validator.New()
 
-func HashPassword()
-
-func VerifyPassword()
 
 func GetUser() gin.HandlerFunc  {
 	return func (c *gin.Context){
-		userId:=c.Param("userId")
-		if err:=helper.MatchUserTypeToUid(c,userId); err!=nil{
-			c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
-			return
+		// if err:=helper.MatchUserTypeToUid(c,userId); err!=nil{
+		// 	c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		// 	return
 
 	}
 	// var ctx,cancel =context.WithTimeout(context.Background(),100*time.Second)
-	var user models.User
-	err:=database.GetDb().First(&user,models.User{Id:userId})
+	// var user models.User
+	// err:=database.GetDb().First(&user,models.User{Id:userId})
 	
-	// defer cancel()
-	if err!=nil{
-		c.JSON(http.StatusInternalServerError,gin.H{"errorMessage":err})
-	}
-	c.JSON(http.StatusOK,user)
+	// // defer cancel()
+	// if err!=nil{
+	// 	c.JSON(http.StatusInternalServerError,gin.H{"errorMessage":err})
+	// }
+	// c.JSON(http.StatusOK,user)
 	// .Count(&count)
 	 
 	}
-}
