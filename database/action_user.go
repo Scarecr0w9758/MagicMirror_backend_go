3f1f603f3dd012b4dcc0812721b93a18bce4171d
demin.m <Matway.demin@yandex.ru>
2024-06-12 23:27:25 +0500

    feat: add validate functuns

--- database/action_user.go
+++ database/action_user.go
@@ -3,12 +3,34 @@ package database
 import (
 	"errors"
 	"magic-mirror/models"
+	"net/mail"
+	"strings"
 )
+
+
+func isStringEmail(str string) bool {
+	index:=strings.Index(str,"@")
+	return index > -1
+}
+
+func validateEmail(email string) bool{
+	_,err:=mail.ParseAddress(email)
+	return err==nil
+
+}
 
 func IsUserExistByLogin(login string) bool {
 	var count = 0 
 	var user models.User
 	GetDb().First(&user,models.User{Login:login}).Count(&count)
+	return count ==1 && user.Id > 0
+}
+
+
+func IsUserExistByEmail(email string) bool {
+	var count = 0 
+	var user models.User
+	GetDb().First(&user,models.User{Email:email}).Count(&count)
 	return count ==1 && user.Id > 0
 }
 
